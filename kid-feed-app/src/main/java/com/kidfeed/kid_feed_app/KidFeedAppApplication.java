2f1382d985b8a85a9125157ad1dca74213ad334f
mkrys <m.kryszewski@gmail.com>
2026-04-17 20:56:03 +0200

    First Commit

---
+++ kid-feed-app/src/main/java/com/kidfeed/kid_feed_app/KidFeedAppApplication.java
@@ -0,0 +1,13 @@
+package com.kidfeed.kid_feed_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidFeedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidFeedAppApplication.class, args);
+	}
+
+}
---
+++ kid-feed-app/src/test/java/com/kidfeed/kid_feed_app/KidFeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidfeed.kid_feed_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidFeedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
