5f8ae2904dcf9a5d4f5f185741aa5984352170be
adam98adam <adamzebr98@gmail.com>
2026-04-24 19:07:47 +0200

    * exclude DataSourceAutoConfiguration

--- kid-feed-app/src/main/java/com/kidfeed/kid_feed_app/KidFeedAppApplication.java
+++ kid-feed-app/src/main/java/com/kidfeed/kid_feed_app/KidFeedAppApplication.java
@@ -2,8 +2,9 @@ package com.kidfeed.kid_feed_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KidFeedAppApplication {
 
 	public static void main(String[] args) {
