ebda5f4e4acfd77389ad1f025e42224cefed36ec
mkrys <m.kryszewski@gmail.com>
2026-04-28 10:17:11 +0200

    Add initial backend structure - entity, repository, dto, mapper, exception, service, controller

--- src/main/java/com/kidfeed/KidFeedAppApplication.java
+++ src/main/java/com/kidfeed/KidFeedAppApplication.java
@@ -2,10 +2,8 @@ package com.kidfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-		//(exclude = {DataSourceAutoConfiguration.class })
 public class KidFeedAppApplication {
 
 	public static void main(String[] args) {
